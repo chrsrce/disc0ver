79026b7c1057a51f357946a4d4e95a95581e5bb1
chrsrce <50304390+chrsrce@users.noreply.github.com>
2024-09-12 23:08:18 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ import requests # type: ignore
 import threading
 import queue
 
+def print_header():
+    print(""" ______   ___   _______  _______  _______  __   __  _______  ______   
+|      | |   | |       ||       ||  _    ||  | |  ||       ||    _ |  
+|  _    ||   | |  _____||       || | |   ||  |_|  ||    ___||   | ||  
+| | |   ||   | | |_____ |       || | |   ||       ||   |___ |   |_||_ 
+| |_|   ||   | |_____  ||      _|| |_|   ||       ||    ___||    __  |
+|       ||   |  _____| ||     |_ |       | |     | |   |___ |   |  | |
+|______| |___| |_______||_______||_______|  |___|  |_______||___|  |_|
+""")
 
 def command_line_arguments():
     parser = argparse.ArgumentParser(
@@ -15,10 +24,25 @@ def command_line_arguments():
     parser.add_argument('-w', '--wordlist')      # option that takes a value
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('-r','--recursion',action='store_true')
+    parser.add_argument('-fs','--filter-size',nargs='+',type=int)
+    parser.add_argument('-fc','--filter-code',nargs='+',type=int)
+    parser.add_argument('-t','--threads',type=int,default=10)
+    parser.add_argument('-o',"--outfile")
     
     args = parser.parse_args()
 
     return args
+
+def print_args(args):
+    print("――――――――――――――――――――――――――――――――――――――――――――――――")
+    print('URL: '+ str(args.URL))
+    print('Wordlist: '+ str(args.wordlist))
+    print('Thread Count: '+str(args.threads))
+    print('Size Filters: ' + ', '.join(map(str, args.filter_size)) if args.filter_size is not None else 'Size Filters: None')
+    print('Response Code Filters: ' + ', '.join(map(str, args.filter_code)) if args.filter_code is not None else 'Response Code Filters: None')
+    print("――――――――――――――――――――――――――――――――――――――――――――――――")
+    print('\n')
+
 
 def import_wordlist(text_file):
     wordlist = []
@@ -36,7 +60,23 @@ for word in wordlist:
 
 lock = threading.Lock()
 
+### https://stackoverflow.com/questions/5419389/how-to-overwrite-the-previous-print-to-stdout
+class Printer:
+    '''
+    Clears the previous line and prints the next.
+    '''
 
+    max_length: int = 0
+
+    def print_replace(self, nextline):
+        '''replace the previous line'''
+        print(f'{nextline}  {' ' * max(0, self.max_length - len(nextline))}', end='\r')
+        self.max_length = max(self.max_length, len(nextline))
+
+    def print_new(self, nextline):
+        '''adds to the print stream'''
+        print(f'{nextline}  {' ' * max(0, self.max_length - len(nextline))}')
+        self.max_length = max(self.max_length, len(nextline))
 
 
 def send_request(url):#add try - except
@@ -44,27 +84,53 @@ def send_request(url):#add try - except
     return response
 
 
+def enumeration(address,args):
 
-def enumeration():
-     while not word_queue.empty():
+    
+
+
+    output_content = True
+    printer = Printer()
+    proxies = {
+        'https': 'https://212.126.5.246:42344'
+    }
+
+    while not word_queue.empty():
         try:
             word = word_queue.get(timeout=1)  # Get a word from the queue
         except queue.Empty:
             break
         
         # Test the word (replace with actual URL and HTTP request logic)
-        url = f"https://google.com/{word}"
-        response = requests.get(url)
+        url = f"{address}/{word}"
+        response = requests.get(url,allow_redirects=False)
         
-        print(f"Thread {threading.current_thread().name} tested {word}: {response.status_code}")
-        
-        # If 200, consider this a valid path and test for recursive paths
+        #Filters
+        if args.filter_size is not None and args.filter_size == len(response.content):
+            output_content= False
+        if args.filter_code and response.status_code in args.filter_code:
+            output_content = False
+            
+            
+        if output_content == True and response.status_code == 200:
+            printer.print_new(f"{word}: {response.status_code} Size: {len(response.content)}")
+        elif output_content == True and response.status_code != 200:
+            printer.print_replace(f"{word}: {response.status_code} Size: {len(response.content)}")
+
+        #if response.status_code in [301,302]:
+         #   redirect_location = response.headers
+         #   print(redirect_location)
+            
         if response.status_code == 200:
             # Use lock to safely add recursive tasks
             with lock:
                 for subword in wordlist:
-                    new_word = f"{word}/{subword}"
+                    new_word = f"{word}/{subword}" 
                     word_queue.put(new_word)  # Add new word to the queue
+            with open(args.outfile,'a') as file:
+                file.write(f"{word}: {response.status_code} Size: {len(response.content)}")
+            
+
 
         word_queue.task_done()
             
@@ -72,13 +138,19 @@ def enumeration():
 
 def main():
     args = command_line_arguments()
+    print_header()
+    print_args(args)
+    
+
+    
     #enumeration(args.wordlist,args.URL,args.recursion)
     wordlist = import_wordlist(args.wordlist)
 
-    num_threads = 10
+
+    num_threads = args.threads
     threads = []
     for i in range(num_threads):
-        t = threading.Thread(target=enumeration,name=f"Thread-{i+1}")
+        t = threading.Thread(target=enumeration,args=(args.URL,args,))
         t.start()
         threads.append(t)
 
