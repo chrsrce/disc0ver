edd5f7db0822cf8edc22e0c42e280ea44114af9f
chrsrce <50304390+chrsrce@users.noreply.github.com>
2024-09-23 18:34:06 +0200

    Add files via upload
    - Added color to the output
    - Added proper redirection handling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+from urllib.parse import urlparse
 import requests # type: ignore
 import threading
 import queue
@@ -24,24 +25,25 @@ def command_line_arguments():
     parser.add_argument('-w', '--wordlist')      # option that takes a value
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('-r','--recursion',action='store_true')
-    parser.add_argument('-p','--proxy',action='store_true')
-    parser.add_argument('-fs','--filter-size',nargs='+',type=int)
-    parser.add_argument('-fc','--filter-code',nargs='+',type=int)
-    parser.add_argument('-t','--threads',type=int,default=10)
-    parser.add_argument('-o',"--outfile")
+    parser.add_argument('--redirect',action='store_true')
+    parser.add_argument('-p','--proxies',help="Text file containing proxies (.txt)") #takes in a proxy list in .txt format
+    parser.add_argument('-fs','--filter-size',nargs='+',type=int,help="Reponses of certain sizes you do not want to see.")
+    parser.add_argument('-fc','--filter-code',nargs='+',type=int,help="Response codes you do not want to see.")
+    parser.add_argument('-t','--threads',type=int,default=10,help="Amount of threads used in sending requests, default: 10")
+    parser.add_argument('-o',"--outfile",help="Output file")
     
     args = parser.parse_args()
 
     return args
 
 def print_args(args):
-    print("――――――――――――――――――――――――――――――――――――――――――――――――")
+    print("\n")
     print('URL: '+ str(args.URL))
     print('Wordlist: '+ str(args.wordlist))
     print('Thread Count: '+str(args.threads))
     print('Size Filters: ' + ', '.join(map(str, args.filter_size)) if args.filter_size is not None else 'Size Filters: None')
     print('Response Code Filters: ' + ', '.join(map(str, args.filter_code)) if args.filter_code is not None else 'Response Code Filters: None')
-    print("――――――――――――――――――――――――――――――――――――――――――――――――")
+    print("--------------------------------------------------")
 
 
 
@@ -71,13 +73,23 @@ class Printer:
 
     def print_replace(self, nextline):
         '''replace the previous line'''
-        print(f'{nextline}  {' ' * max(0, self.max_length - len(nextline))}', end='\r')
+        print(f'{nextline}  {" " * max(0, self.max_length - len(nextline))}', end='\r')
         self.max_length = max(self.max_length, len(nextline))
 
     def print_new(self, nextline):
         '''adds to the print stream'''
-        print(f'{nextline}  {' ' * max(0, self.max_length - len(nextline))}')
+        print(f'{nextline}  {" " * max(0, self.max_length - len(nextline))}')
         self.max_length = max(self.max_length, len(nextline))
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 
 def send_request(url):#add try - except
@@ -86,7 +98,12 @@ def send_request(url):#add try - except
 
 
 def enumeration(address,args):
-    proxy_list = import_wordlist("valid_proxies.txt")
+
+    if args.proxies is not None:
+        proxy_list = import_wordlist("valid_proxies.txt")
+        proxies = {"http": random.choice(proxy_list), "https": random.choice(proxy_list)} if args.proxy else None
+    else:
+        proxies = None
     output_content = True
     printer = Printer()
 
@@ -98,10 +115,9 @@ def enumeration(address,args):
         
         # Test the word (replace with actual URL and HTTP request logic)
         url = f"{address}/{word}"
-        proxies = {"http": random.choice(proxy_list), "https": random.choice(proxy_list)} if args.proxy else None
 
         try:
-            response = requests.get(url, allow_redirects=False, proxies=proxies)
+            response = requests.get(url, allow_redirects=args.redirect, proxies=proxies)
         except:
             continue
 
@@ -113,29 +129,64 @@ def enumeration(address,args):
         if args.filter_code and response.status_code in args.filter_code:
             output_content = False
             
-            
-        if output_content == True and response.status_code == 200:
-            printer.print_new(f"{word}: {response.status_code} Size: {len(response.content)}")
-        elif output_content == True and response.status_code != 200:
-            printer.print_replace(f"{word}: {response.status_code} Size: {len(response.content)}")
+        if output_content == True and response.status_code == 200 and response.history:
+            # Get the final redirect location from the last response in the history
+            final_redirect = response.history[-1].headers.get('Location', 'Unknown')
+            if "http" not in final_redirect:
+                final_redirect = final_redirect.replace("/", "")
+            else:
+                parsed_url = urlparse(url)
+                path_parts = parsed_url.path.split('/')
+                desired_word = path_parts[-1] if path_parts[-1] else path_parts[-2]  # Handle trailing slash case
 
-        #if response.status_code in [301,302]:
-         #   redirect_location = response.headers
-         #   print(redirect_location)
-            
+                if url in final_redirect:
+                    final_redirect = desired_word
+            if word == final_redirect.replace("/",""):
+                printer.print_new(f"{bcolors.OKGREEN}{word}: {response.status_code} Size: {len(response.content)}{bcolors.ENDC}")
+                output_to_outfile(word,response,False,None)
+            else:
+                printer.print_new(f"{bcolors.OKCYAN}{word}: 301 --> {final_redirect} Size: {len(response.content)}{bcolors.ENDC}")
+                output_to_outfile(word,response,True,final_redirect)    
+                
+        elif output_content == True and response.status_code == 200:
+            printer.print_new(f"{bcolors.OKGREEN}{word}: {response.status_code} Size: {len(response.content)}{bcolors.ENDC}")
+            output_to_outfile(word,response,False,None)
+       
+        elif output_content == True and (response.status_code == 404 or response.status_code == 403):
+            printer.print_replace(f"{bcolors.RED}{word}: {response.status_code} Size: {len(response.content)}{bcolors.ENDC}")
+       
+        elif output_content == True and response.status_code in [301, 302]:
+            redirect_location = response.headers.get('Location', 'Unknown')
+            printer.print_replace(f"{bcolors.OKCYAN}{word}: {response.status_code} -->  {redirect_location} Size: {len(response.content)}{bcolors.ENDC}")
+            if args.redirect:
+                output_to_outfile(word,response,True,None)
+
+        elif output_content == True and response.status_code != 200:
+            printer.print_replace(f"{bcolors.YELLOW}{word}: {response.status_code} Size: {len(response.content)}{bcolors.ENDC}")
+        
+
+
         if response.status_code == 200:
             # Use lock to safely add recursive tasks
             with lock:
                 for subword in wordlist:
-                    new_word = f"{word}/{subword}" 
-                    word_queue.put(new_word)  # Add new word to the queue
-            with open(args.outfile,'a') as file:
-                file.write(f"{word}: {response.status_code} Size: {len(response.content)}")
+                    if final_redirect in url:
+                        continue
+                    else: 
+                        new_word = f"{final_redirect}/{subword}" 
+                        word_queue.put(new_word)  # Add new word to the queue
+
             
 
 
         word_queue.task_done()
-            
+
+def output_to_outfile(word,response,redirect,final_redirect):
+    with open(args.outfile,'a') as file:
+            if redirect:
+                file.write(f"{word}: 301 --> {final_redirect} Size: {len(response.content)}\n")     
+            else:
+                file.write(f"{word}: {response.status_code} Size: {len(response.content)}\n")      
 
 
 def main():
